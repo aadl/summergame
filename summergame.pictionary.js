86fb57032a5c7a690a506786537b4b34d40a0781
ejk <eric@rhymeswithtoaster.com>
2011-05-31 11:05:54 -0400

    Renamed Pictionary to Trivia

--- summergame.pictionary.js
+++ summergame.pictionary.js
@@ -1,5 +1,5 @@
-function pictionaryupdate() {
-  $.getJSON('http://odie.aadl.org/~kloostere/beta/?q=summergame/pictionaryupdate', function(json) {
+function triviaupdate() {
+  $.getJSON('http://odie.aadl.org/~kloostere/beta/?q=summergame/triviaupdate', function(json) {
     $("div.autorefresh_div").hide().fadeIn("slow").html(json['html']);
   });
 }
