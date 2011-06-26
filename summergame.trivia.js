710f26f2089945a8a259ee9b9a00e336794437a9
ejk <eric@rhymeswithtoaster.com>
2011-06-25 23:53:00 -0400

    Hard code json update path

--- summergame.trivia.js
+++ summergame.trivia.js
@@ -1,5 +1,5 @@
 function triviaupdate() {
-  $.getJSON('http://odie.aadl.org/~kloostere/beta/?q=summergame/triviaupdate', function(json) {
+  $.getJSON('http://play.aadl.org/summergame/triviaupdate', function(json) {
     $("div.autorefresh_div").hide().fadeIn("slow").html(json['html']);
   });
 }
