8c453d48bff8317cc4a803af5231e3c293cf8dba
Troy Leonard <leonardt@aadl.org>
2016-06-22 16:27:56 -0400

    Modified form to prevent it being submitted without addressing whether a catalog item should be tagged

---
+++ summergame.staff.forms.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+	var bibField = document.getElementById('edit-tag-bib');
+	$('#summergame-code-edit-form input:radio').change(function() {
+		if ($(this).val() == 'tag_bib_yes') {
+			bibField.required = true;
+			bibField.disabled = false;
+		}
+		else {
+			bibField.required = false;
+			bibField.disabled = true;
+		}
+	});
+});
