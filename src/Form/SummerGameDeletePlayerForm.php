11db3c57e579d9c0a383fe5a25a131db89410e14
ejk <eric@rhymeswithtoaster.com>
2018-06-15 01:18:27 -0400

    Add player delete form

---
+++ src/Form/SummerGameDeletePlayerForm.php
@@ -0,0 +1,81 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\accountfix\Form\SummerGameDeletePlayerForm
+ */
+
+namespace Drupal\summergame\Form;
+
+use Drupal\Core\Form\FormBase;
+use Drupal\Core\Form\FormStateInterface;
+
+class SummerGameDeletePlayerForm extends FormBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormId() {
+    return 'summergame_delete_player_form';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state, $pid = 0) {
+    $pid = (int) $pid;
+    if (summergame_player_access($pid)) {
+      if ($player = summergame_player_load(['pid' => $pid])) {
+        $form = [];
+        $form['player'] = [
+          '#type' => 'value',
+          '#value' => $player,
+        ];
+        $player_name = ($player['nickname'] ? $player['nickname'] : $player['name']);
+        $form['warning'] = [
+          '#markup' => "Are you sure you want to delete player $player_name? All associated scores and badges will be deleted as well. This action cannot be undone.",
+        ];
+        $form['inline'] = [
+          '#prefix' => '<div class="container-inline">',
+          '#suffix' => '</div>',
+        ];
+        $form['inline']['submit'] = [
+          '#type' => 'submit',
+          '#value' => t('Delete'),
+          '#prefix' => '<div class="sg-form-actions">'
+        ];
+        $form['inline']['cancel'] = [
+          '#type' => 'link',
+          '#title' => $this->t('Cancel'),
+          '#url' => \Drupal\Core\Url::fromRoute('summergame.player', ['pid' => $pid]),
+          '#suffix' => '</div>'
+        ];
+
+        return $form;
+      }
+      else {
+        // No Player ID
+        drupal_set_message("Unable to find player entry", 'error');
+        return $this->redirect('summergame.player');
+      }
+    }
+    else {
+      // No access to Player
+      drupal_set_message("Unable to access player", 'error');
+      return $this->redirect('summergame.player');
+    }
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    $player = $form_state->getValue('player');
+    summergame_player_delete(['pid' => $player['pid']]);
+    $player_name = ($player['nickname'] ? $player['nickname'] : $player['name']);
+    drupal_set_message("Deleted player $player_name and all associated points and badges");
+    $form_state->setRedirect('summergame.player');
+
+    return;
+  }
+}
