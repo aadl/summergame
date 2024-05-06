5e8af4bb9b8a87e49b26c40ee6e11d23b6ea1b05
Matthew Swinehart <mdswinehart@gmail.com>
2024-05-06 13:54:55 -0400

    Fix formatting on connectioncontroller

--- src/Controller/ConnectionController.php
+++ src/Controller/ConnectionController.php
@@ -2,49 +2,44 @@
 
 /**
  * @file
- * Contains \Drupal\summergame\Controller\TriviaController.
+ * Contains \Drupal\summergame\Controller\ConnectionController.
  */
 
 namespace Drupal\summergame\Controller;
 
-use Drupal\Core\Controller\ControllerBase;
 use Drupal\Core\Routing\TrustedRedirectResponse;
-use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\RedirectResponse;
-
-//use Drupal\Core\Database\Database;
-//use Drupal\Core\Url;
 
 /**
  * External connection controller for the Summer Game module.
  */
 class ConnectionController extends PlayerController
 {
-	public function show()
-	{
-		$user = \Drupal::currentUser();
-		if (!$user->isAuthenticated()) {
-			return new RedirectResponse('/user/login?destination=/summergame/scatterlog/connect');
-		}
-		$uid = $user->id();
-		$db = \Drupal::database();
-		$result = $db->query('SELECT * FROM sg_players WHERE uid = ' . (int) $uid . ' ORDER BY pid ASC');
-		$players = [];
-		while ($player = $result->fetchAssoc()) {
-			$players[] = $player;
-		}
-		return [
-			'#cache' => [
-				'max-age' => 0, // Don't cache, always get fresh data
-			],
-			'#theme' => 'summergame_player_external_redeem',
-			'#uid' => $uid,
-			'#players' => $players,
-		];
-	}
-	public function connect($uid)
-	{
-		$scatterlogKey = \Drupal::config('summergame.settings')->get('summergame_scatterlog_key');
-		return new TrustedRedirectResponse(\Drupal::config('summergame.settings')->get('summergame_scatterlog_url') . '/connect?uid=' . $uid . '&key=' . $scatterlogKey);
-	}
+  public function show()
+  {
+    $user = \Drupal::currentUser();
+    if (!$user->isAuthenticated()) {
+      return new RedirectResponse('/user/login?destination=/summergame/scatterlog/connect');
+    }
+    $uid = $user->id();
+    $db = \Drupal::database();
+    $result = $db->query('SELECT * FROM sg_players WHERE uid = ' . (int) $uid . ' ORDER BY pid ASC');
+    $players = [];
+    while ($player = $result->fetchAssoc()) {
+      $players[] = $player;
+    }
+    return [
+      '#cache' => [
+        'max-age' => 0, // Don't cache, always get fresh data
+      ],
+      '#theme' => 'summergame_player_external_redeem',
+      '#uid' => $uid,
+      '#players' => $players,
+    ];
+  }
+  public function connect($uid)
+  {
+    $scatterlogKey = \Drupal::config('summergame.settings')->get('summergame_scatterlog_key');
+    return new TrustedRedirectResponse(\Drupal::config('summergame.settings')->get('summergame_scatterlog_url') . '/connect?uid=' . $uid . '&key=' . $scatterlogKey);
+  }
 }
